89713370fe4119dfd165b8a46ad907092868fffa
Charlie Root <root@infra-otrs.traditionsa.lu>
2021-07-09 16:46:21 +0200

    Added a second customer user backend as an address book.

--- Kernel/Config.pm
+++ Kernel/Config.pm
@@ -70,6 +70,99 @@ sub Load {
     # $Self->{SessionUseCookie} = 0;
     # $Self->{CheckMXRecord} = 0;
 
+    $Self->{'AuthModule'} = 'Kernel::System::Auth::HTTPBasicAuth';
+    $Self->{'Customer::AuthModule'} = 'Kernel::System::CustomerAuth::HTTPBasicAuth';
+    $Self->{'CustomerPanelCreateAccount'} = 0;
+    $Self->{'CustomerPanelLostPassword'} = 0;
+
+    $Self->{CustomerUser1} = {
+        Name   => 'Address Book',
+        Module => 'Kernel::System::CustomerUser::DB',
+        Params => {
+            Table => 'ext.address_book',
+            SearchCaseSensitive => 0,
+        },
+
+        # customer unique id
+        CustomerKey => 'email',
+
+        # customer #
+        CustomerID    => 'customer_id',
+        CustomerValid => 'valid_id',
+
+        # The last field must always be the email address so that a valid
+        #   email address like "John Doe" <john.doe@domain.com> can be constructed from the fields.
+        CustomerUserListFields => [ 'full_name', 'email' ],
+
+#        CustomerUserListFields => ['login', 'first_name', 'last_name', 'customer_id', 'email'],
+        CustomerUserSearchFields           => [ 'full_name', 'customer_id', 'email' ],
+        CustomerUserSearchPrefix           => '*',
+        CustomerUserSearchSuffix           => '*',
+        CustomerUserSearchListLimit        => 250,
+        CustomerUserPostMasterSearchFields => [ 'email' ],
+        CustomerUserNameFields             => [ 'full_name' ],
+        CustomerUserEmailUniqCheck         => 1,
+
+#        # Configures the character for joining customer user name parts. Join single space if it is not defined.
+#        # CustomerUserNameFieldsJoin => '',
+
+#        # show now own tickets in customer panel, CompanyTickets
+#        CustomerUserExcludePrimaryCustomerID => 0,
+#        # generate auto logins
+#        AutoLoginCreation => 0,
+#        # generate auto login prefix
+#        AutoLoginCreationPrefix => 'auto',
+#        # admin can change customer preferences
+#        AdminSetPreferences => 1,
+        # use customer company support (reference to company, See CustomerCompany settings)
+        CustomerCompanySupport => 0,
+        # cache time to live in sec. - cache any database queries
+        CacheTTL => 60 * 60 * 24,
+#        # Consider this source read only.
+#        ReadOnly => 1,
+        Map => [
+
+            # Info about dynamic fields:
+            #
+            # Dynamic Fields of type CustomerUser can be used within the mapping (see example below).
+            # The given storage (third column) then can also be used within the following configurations (see above):
+            # CustomerUserSearchFields, CustomerUserPostMasterSearchFields, CustomerUserListFields, CustomerUserNameFields
+            #
+            # Note that the columns 'frontend' and 'readonly' will be ignored for dynamic fields.
+
+            # note: Login, Email and CustomerID needed!
+            # var, frontend, storage, shown (1=always,2=lite), required, storage-type, http-link, readonly, http-link-target, link class(es)
+            [ 'UserName',    'Name',           'full_name',     1, 1, 'var', '', 0, undef, undef ],
+            [ 'UserLogin',        'Username',            'login',          1, 1, 'var', '', 0, undef, undef ],
+            [ 'UserEmail',        'Email',               'email',          1, 1, 'var', '', 0, undef, undef ],
+#            [ 'UserEmail',        'Email',               'email',          1, 1, 'var', '[% Env("CGIHandle") %]?Action=AgentTicketCompose;ResponseID=1;TicketID=[% Data.TicketID | uri %];ArticleID=[% Data.ArticleID | uri %]', 0, '', 'AsPopup OTRSPopup_TicketAction' ],
+            [ 'UserCustomerID',   'CustomerID',          'customer_id',    0, 1, 'var', '', 0, undef, undef ],
+#            [ 'UserCustomerIDs',  'CustomerIDs',         'customer_ids',   1, 0, 'var', '', 0, undef, undef ],
+	    #[ 'UserPhone',        'Phone',               'phone',          1, 0, 'var', '', 0, undef, undef ],
+	    #[ 'UserFax',          'Fax',                 'fax',            1, 0, 'var', '', 0, undef, undef ],
+	    #[ 'UserMobile',       'Mobile',              'mobile',         1, 0, 'var', '', 0, undef, undef ],
+	    #[ 'UserStreet',       'Street',              'street',         1, 0, 'var', '', 0, undef, undef ],
+	    #[ 'UserZip',          'Zip',                 'zip',            1, 0, 'var', '', 0, undef, undef ],
+	    #[ 'UserCity',         'City',                'city',           1, 0, 'var', '', 0, undef, undef ],
+	    #[ 'UserCountry',      'Country',             'country',        1, 0, 'var', '', 0, undef, undef ],
+            [ 'UserComment',      'Comment',             'comments',       1, 0, 'var', '', 0, undef, undef ],
+            [ 'ValidID',          'Valid',               'valid_id',       0, 1, 'int', '', 0, undef, undef ],
+
+            # Dynamic field example
+#            [ 'DynamicField_Name_X', undef, 'Name_X', 0, 0, 'dynamic_field', undef, 0, undef, undef ],
+        ],
+
+        # default selections
+        Selections => {
+
+#            UserTitle => {
+#                'Mr.' => 'Mr.',
+#                'Mrs.' => 'Mrs.',
+#            },
+        },
+    };
+
+
     # ---------------------------------------------------- #
 
     # ---------------------------------------------------- #
