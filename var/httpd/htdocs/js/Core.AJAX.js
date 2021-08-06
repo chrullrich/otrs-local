948345a03330b14fba54dc5a3c574491fc365d28
Christian Ullrich <chris@chrullrich.net>
2021-08-06 08:42:26 +0200

    Imported vendor 6.1.1

--- var/httpd/htdocs/js/Core.AJAX.js
+++ var/httpd/htdocs/js/Core.AJAX.js
@@ -629,5 +629,36 @@ Core.AJAX = (function (TargetNS) {
         });
     };
 
+    TargetNS.FunctionCallSynchronous = function (URL, Data, Callback, DataType) {
+
+        // store the original state
+        // this is basically an example how to access
+        // the current state of the $.ajaxSetup values
+        var OriginalAsyncState = $.ajaxSetup()['async'];
+
+        // make a custom callback that gets passed to the standard Core.AJAX.FunctionCall
+        // that resets back to asynchronous AJAX calls as before and executes the regualar
+        // given Callback function as usual
+        var ResetCallback = function (Response) {
+
+            // set requests back to asynchronous
+            $.ajaxSetup({
+                async: OriginalAsyncState
+            });
+
+            // call given callback function as usual
+            Callback(Response);
+        };
+
+        // set this request as synchronous
+        $.ajaxSetup({
+            async: false
+        });
+
+        // start the wanted request by the framework functionality with our
+        // manipulated callback function and disabled async flag
+        Core.AJAX.FunctionCall(URL, Data, ResetCallback, DataType);
+    };
+
     return TargetNS;
 }(Core.AJAX || {}));
